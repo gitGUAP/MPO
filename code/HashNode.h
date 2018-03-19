5f1ad0d7a787274d927f5c2ab76d3375cfdfa5fb
Sergey <grishy.ru@yandex.ru>
2018-03-19 21:31:12 +0300

    set lab3 tabel

--- code/HashNode.h
+++ code/HashNode.h
@@ -1,16 +1,15 @@
 #pragma once
 #include <string>
 
-template <typename V>
-class HashNode {
+template <typename V> class HashNode {
 public:
-	HashNode(const std::string &key, const V &value):
-		key(key), value(value) {}
+  HashNode(const std::string &key, const V &value) : key(key), value(value) {}
 
-	std::string getKey() const { return key; }
-	V getValue() const { return value; }
-	void setValue(V val) { HashNode::value = val; }
+  std::string getKey() const { return key; }
+  V getValue() const { return value; }
+  void setValue(V val) { HashNode::value = val; }
+
 private:
-	std::string key;
-	V value;
+  std::string key;
+  V value;
 };
